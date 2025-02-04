cb2dad87808561827daf6d6e3b728da87f9fbefd
stephani.miler <107444034+StephaniMiler@users.noreply.github.com>
2025-02-04 18:24:24 -0300

    02-04-25

---
+++ 028.Programming logic 1/introduction.js
@@ -0,0 +1,15 @@
+//alert("hello, world!");
+
+/*let name = prompt("What is your name?");
+
+alert("Hello, " + name);*/
+
+let num1 = prompt("Enter a number: ")
+num1 = parseFloat(num1)
+let num2 = prompt("Enter another number: ")
+num2 = parseFloat(num2)
+
+
+
+alert("The sum of the numbers entered is: " +  (num1 + num2) + " !")
+

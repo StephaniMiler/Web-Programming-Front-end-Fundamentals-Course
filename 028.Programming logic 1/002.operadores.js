422c37d120f8eadc1a5bef5c84f36731fd9844ea
stephani.miler <107444034+StephaniMiler@users.noreply.github.com>
2025-02-05 13:10:52 -0300

    02-05-25

---
+++ 028.Programming logic 1/002.operadores.js
@@ -0,0 +1,57 @@
+/*Pedir para o usuário digitar dois números e mostrar
+-A soma;
+-A subtração;
+-A multiplicação;
+-A divisão*/
+
+/*let n1 = prompt("Digite um número: ")
+n1 = parseInt(n1)
+let n2 = prompt("Digite outro número: ")
+n2 = parseInt(n2)
+
+alert("A soma dos dois números é: " + (n1+n2))
+alert("A subtração dos dois números é: " + (n1-n2))
+alert("A multiplicação dos dois números é: " + (n1*n2))
+alert("A divisão dos dois números é: " + (n1/n2))*/
+
+
+/*Pedir para o usuário digitar dois números e mostrar a média*/
+
+
+/*let n3 = prompt("Digite um número: ")
+n1 = parseInt(n3)
+let n4 = prompt("Digite outro número: ")
+n2 = parseInt(n4)
+
+alert("A média dos dois números é: " + ((n1+n2)/2));*/
+
+// ATENÇÃO: PARA ESSE EXERCÍCIO, VOCÊ PODE CRIAR APENAS UMA ÚNICA VARIÁVEL
+
+        // crie uma variável e atribua o valor 10
+        let n = 10
+
+        // some 5 à essa variável e reatribua o valor
+        n += 5
+
+        // multiplique o valor da variável por 5 e reatribua o valor
+        n *= 5
+
+        // some 1 à variável e reatribua o novo valor
+        n++
+
+        // sem alterar o valor da variável, mostre na tela o resto da divisão por 6
+        // a mensagem deve ser: O resto da divisão de (variavel) por 6 é (resultado)
+        alert("O resultado da divisão de " + n + " por 6 é " + (n % 6))
+
+        // sem alterar o valor da variável, mostre na tela o resto da divisão por 6 elevado ao quadrado
+        // a mensagem deve ser: O quadrado do resto da divisão de (variavel) por 6 é (resultado)
+        alert("O quadrado do resto da divisão de " + n + " por 6 é " + ((n % 6) ** 2))
+
+        //mostre na tela o valor da variavel
+        alert(n)
+
+        // agora divida o valor da variavel por 4 e reatribua o valor
+        n /= 4
+
+        // Qual o valor da variavel ?
+        alert(n)

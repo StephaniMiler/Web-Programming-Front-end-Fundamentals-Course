36cb4cef1e999b807a8ccfe4ccd759cbca3f1674
stephani.miler <107444034+StephaniMiler@users.noreply.github.com>
2025-02-13 15:03:10 -0300

    02-13-25

---
+++ 039. JS- Arrays/006.array06.js
@@ -0,0 +1,9 @@
+//Array.from() – Cria um array a partir de algo semelhante a um array (como uma string ou um objeto iterável).
+let texto = "Hello";
+let arrayLetras = Array.from(texto);
+
+console.log(arrayLetras); // ['H', 'e', 'l', 'l', 'o']
+
+//Array.of() – Cria um array a partir dos argumentos passados.
+let numeros = Array.of(1, 2, 3, 4, 5);
+console.log(numeros); // [1, 2, 3, 4, 5]

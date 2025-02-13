f760feb7c6e26b5f4c5ba54edb669f8b1adef0ff
stephani.miler <107444034+StephaniMiler@users.noreply.github.com>
2025-02-13 13:22:05 -0300

    02-13-25

--- 039. JS- Arrays/002.array02.js
+++ 039. JS- Arrays/002.array02.js
@@ -1,6 +1,13 @@
 let arr = [4, 5, 10, 20, 35, 4, 5]
 
 console.log(arr.lastIndexOf(5)) //6
+
 console.log(arr.includes(78)) //false
 
+console.log(arr.find(function(el){
+    return el > 10
+
+})) //20, 35
+
+console.log(arr.findIndex(10)) //2
 

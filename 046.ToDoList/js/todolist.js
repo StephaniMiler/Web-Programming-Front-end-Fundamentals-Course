311689369a4073de97780177dddca2c59c679eb2
Stephani Sara Miler <stephanisaramiler@gmail.com>
2025-02-16 08:40:55 -0300

    02-16-25

--- 046.ToDoList/js/todolist.js
+++ 046.ToDoList/js/todolist.js
@@ -15,20 +15,62 @@
         }
     ]
 
-    function addEventLi(li){
+   /* function addEventLi(li){
         li.addEventListener("click", function(){
             console.log(this)
         })
-    }
+    }*/
 
     function generateLiTask(obj){
         const li = document.createElement("li")
-        li.className = "todo-item"
         const p = document.createElement("p")
+        const checkButton = document.createElement("button")
+        const editButton = document.createElement("i")
+        const deleteButton = document.createElement("i")
+
+        li.className = "todo-item"
+
+        checkButton.className ="button-check"
+        checkButton.innerHTML = '<i class= "fas fa-check displayNone"></i>'
+        checkButton.setAttribute("data-action", "checkButton")
+
+        li.appendChild(checkButton)
+        
         p.className = "task-name"
         p.textContent = obj.name
         li.appendChild(p)
-        addEventLi(li)
+
+        editButton.className = "fas fa-edit"
+        editButton.setAttribute("data-action", "editButton")
+        li.appendChild(editButton)
+
+        const containerEdit = document.createElement("div")
+        containerEdit.className = "editContainer"
+        const inputEdit = document.createElement("input")
+        inputEdit.setAttribute("type", "text")
+        inputEdit.className = "editInput"
+        containerEdit.appendChild(inputEdit)
+
+        const containerEditButton = document.createElement("button")
+        containerEditButton.className = "editButton"
+        containerEditButton.textContent = "Edit"
+        containerEditButton.setAttribute("data-action", "containerEditButton")
+        containerEdit.appendChild(containerEditButton)
+
+        const containerCancelButton = document.createElement("button")
+        containerCancelButton.className = "cancelButton"
+        containerCancelButton.textContent = "Cancel"
+        containerCancelButton.setAttribute("data-action", "containerCancelButton")
+        containerEdit.appendChild(containerCancelButton)
+
+        li.appendChild(containerEdit)
+
+
+        deleteButton.className = "fas fa-trash-alt"
+        deleteButton.setAttribute("data-action", "deleteButton")
+        li.appendChild(deleteButton)
+
+       // addEventLi(li)
         return li
     }
 
@@ -48,6 +90,11 @@
         })
     }
 
+    function clickedUl(e){
+        console.log(e.target)
+        console.log(e.target.getAttribute("data-action"))
+    }
+
     todoAddForm.addEventListener("submit", function(e){
         e.preventDefault()
 
@@ -59,10 +106,12 @@
         `*/
         addTask(itemInput.value)
         renderTasks()
-        
+
         itemInput.value = ""
         itemInput.focus()
     });
+
+    ul.addEventListener("click", clickedUl)
 
     renderTasks()
 

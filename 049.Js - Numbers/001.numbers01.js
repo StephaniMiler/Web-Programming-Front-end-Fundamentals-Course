f62e5f9980d9147d3afaa248c6da7010fb981c70
stephani.miler <107444034+StephaniMiler@users.noreply.github.com>
2025-02-18 13:09:46 -0300

    02-18-25

---
+++ 049.Js - Numbers/001.numbers01.js
@@ -0,0 +1,14 @@
+// Exemplo de toPrecision()
+let numPrecision = 123.456;
+console.log(numPrecision.toPrecision(4)); // "123.5" (arredonda para 4 dígitos no total)
+console.log(numPrecision.toPrecision(6)); // "123.456" (mantém os 6 dígitos no total)
+
+// Exemplo de toFixed()
+let numFixed = 123.456;
+console.log(numFixed.toFixed(2)); // "123.46" (arredonda para 2 casas decimais)
+console.log(numFixed.toFixed(0)); // "123" (sem casas decimais, arredonda para inteiro)
+
+// Exemplo de toExponential()
+let numExponential = 123.456;
+console.log(numExponential.toExponential(2)); // "1.23e+2" (notação científica com 2 casas decimais)
+console.log(numExponential.toExponential(4)); // "1.2346e+2" (notação científica com 4 casas decimais)
